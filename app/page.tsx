a807ef4d89e1769b98a3c5269cd0a787d64e823c
0xDeepanshu <wtfdeep@duck.com>
2025-10-11 23:54:13 +0530

    fixed wellknkown

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ import type { Metadata } from 'next'
 import FrontEnd from '@/components/pages/main'
 const frame = {
   version: 'next',
-  imageUrl: `${APP_URL}/images/EVMcd.webp`,
+  imageUrl: `${APP_URL}/images/EVMcd.png`,
   button: {
     title: 'Launch Template',
     action: {
       type: 'launch_frame',
       name: 'Stake Stack',
       url: APP_URL,
-      splashImageUrl: `${APP_URL}/images/BlackRL.webp`,
+      splashImageUrl: `${APP_URL}/images/BlackRL.png`,
       splashBackgroundColor: '#f7f7f7',
     },
   },
