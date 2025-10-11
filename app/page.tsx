bf25b969ab11cbd286a5a40866e7bd4f111a6961
0xDeepanshu <wtfdeep@duck.com>
2025-10-12 02:54:55 +0530

    fixed back

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,38 @@
-'use client'
-
-import { useEffect, useState } from 'react'
-import { sdk } from '@farcaster/miniapp-sdk'
 import App from '@/components/pages/app'
+import { APP_URL } from '@/lib/constants'
+import type { Metadata } from 'next'
 import FrontEnd from '@/components/pages/main'
+import { useMiniApp } from '@neynar/react';
+const miniapp = {
+  version: '1',
+  imageUrl: `${APP_URL}/images/EVMcd.png`,
+  button: {
+    title: 'Launch Template',
+    action: {
+      type: 'launch_miniapp',
+      name: 'Stake Stack',
+      url: APP_URL,
+      splashImageUrl: `${APP_URL}/images/BlackRL.png`,
+      splashBackgroundColor: '#f7f7f7',
+    },
+  },
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const { actions, added, notificationDetails, haptics, context } =
+    useMiniApp();
+  return {
+    title: 'Rupture Labs mini app ',
+    openGraph: {
+      title: 'Rupture Labs MiniApp ',
+      description: 'A Rupture Labs gaming formation',
+    },
+    other: {
+      'fc:miniapp': JSON.stringify(miniapp),
+    },
+  }
+}
 
 export default function Home() {
-  const [isMiniApp, setIsMiniApp] = useState<boolean | null>(null)
-
-  useEffect(() => {
-    async function checkMiniApp() {
-      try {
-        const insideMiniApp = await sdk.isInMiniApp()
-        setIsMiniApp(insideMiniApp)
-      } catch (err) {
-        console.error('Error checking Mini App environment:', err)
-        setIsMiniApp(false)
-      }
-    }
-
-    checkMiniApp()
-  }, [])
-
-  // Loading state (optional)
-  if (isMiniApp === null) return <div>Loading...</div>
-
-  // Conditional rendering
-  return isMiniApp ? <App /> : <FrontEnd />
+  return <App />
 }
