333fb969ffa073820cee46d43ba6f58948677492
0xDeepanshu <wtfdeep@duck.com>
2025-10-12 00:38:01 +0530

    changed manifest

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -4,20 +4,19 @@ import { APP_URL } from "../../../lib/constants";
 export async function GET() {
   const farcasterConfig = {
     // TODO: Add your own account association
-    frame: {
-      version: "1",
-      name: "Rupture Labs mini app",
-      iconUrl: `${APP_URL}/images/BlackRL.png`,
-      homeUrl: `${APP_URL}`,
-      imageUrl: `${APP_URL}/images/EVMcd.png`,
-      screenshotUrls: [],
-      tags: ["monad", "farcaster", "miniapp", "template"],
-      primaryCategory: "developer-tools",
-      buttonTitle: "Launch Template",
-      splashImageUrl: `${APP_URL}/images/BlackRL.png`,
-      splashBackgroundColor: "#ffffff",
-      webhookUrl: `${APP_URL}/api/webhook`,
-    },
+    "frame": {
+    "name": "rupture labs mini games",
+    "version": "1",
+    "iconUrl": "https://stakcstakefarcaster.vercel.app/icon.png",
+    "homeUrl": "https://stakcstakefarcaster.vercel.app",
+    "imageUrl": "https://stakcstakefarcaster.vercel.app/image.png",
+    "splashImageUrl": "https://stakcstakefarcaster.vercel.app/splash.png",
+    "splashBackgroundColor": "#FFFFFF",
+    "webhookUrl": "https://stakcstakefarcaster.vercel.app/api/webhook",
+    "subtitle": "stack stale",
+    "description": "experience the fututre of gaming",
+    "primaryCategory": "games"
+  }
   };
 
   return NextResponse.json(farcasterConfig);
