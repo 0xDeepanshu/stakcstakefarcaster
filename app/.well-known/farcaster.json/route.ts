8ff6396b366cf04fad06b6a174128f7e2c5bc4f2
0xDeepanshu <wtfdeep@duck.com>
2025-10-12 00:46:36 +0530

    fixed header and payload

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -3,20 +3,26 @@ import { APP_URL } from "../../../lib/constants";
 
 export async function GET() {
   const farcasterConfig = {
-    // TODO: Add your own account association
-    "frame": {
-    "name": "rupture labs mini games",
-    "version": "1",
-    "iconUrl": "https://stakcstakefarcaster.vercel.app/icon.png",
-    "homeUrl": "https://stakcstakefarcaster.vercel.app",
-    "imageUrl": "https://stakcstakefarcaster.vercel.app/image.png",
-    "splashImageUrl": "https://stakcstakefarcaster.vercel.app/splash.png",
-    "splashBackgroundColor": "#FFFFFF",
-    "webhookUrl": "https://stakcstakefarcaster.vercel.app/api/webhook",
-    "subtitle": "stack stale",
-    "description": "experience the fututre of gaming",
-    "primaryCategory": "games"
-  }
+    
+  "accountAssociation": {
+    "header": "eyJmaWQiOjkxNTIsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgwMmVmNzkwRGQ3OTkzQTM1ZkQ4NDdDMDUzRURkQUU5NDBEMDU1NTk2In0",
+    "payload": "eyJkb21haW4iOiJyZXdhcmRzLndhcnBjYXN0LmNvbSJ9",
+    "signature": "MHgxMGQwZGU4ZGYwZDUwZTdmMGIxN2YxMTU2NDI1MjRmZTY0MTUyZGU4ZGU1MWU0MThiYjU4ZjVmZmQxYjRjNDBiNGVlZTRhNDcwNmVmNjhlMzQ0ZGQ5MDBkYmQyMmNlMmVlZGY5ZGQ0N2JlNWRmNzMwYzUxNjE4OWVjZDJjY2Y0MDFj"
+  },
+    frame: {
+      version: "1",
+      name: "Rupture Labs mini app",
+      iconUrl: `${APP_URL}/images/BlackRL.png`,
+      homeUrl: `${APP_URL}`,
+      imageUrl: `${APP_URL}/images/EVMcd.png`,
+      screenshotUrls: [],
+      tags: ["monad", "farcaster", "miniapp", "template"],
+      primaryCategory: "developer-tools",
+      buttonTitle: "Launch Template",
+      splashImageUrl: `${APP_URL}/images/BlackRL.png`,
+      splashBackgroundColor: "#ffffff",
+      webhookUrl: `${APP_URL}/api/webhook`,
+    },
   };
 
   return NextResponse.json(farcasterConfig);
--- error.md
+++ error.md
@@ -1,10 +1,4 @@
-Failed to compile.
-./lib/utils.ts:1:39
-Type error: Cannot find module 'clsx' or its corresponding type declarations.
-> 1 | import { clsx, type ClassValue } from "clsx"
-    |                                       ^
-  2 | import { twMerge } from "tailwind-merge"
-  3 |
-  4 | export function cn(...inputs: ClassValue[]) {
- ELIFECYCLE  Command failed with exit code 1.
-Error: Command "pnpm run build" exited with 1
+AddMiniApp.InvalidDomainManifest: Invalid domain manifest
+
+
+tell 
