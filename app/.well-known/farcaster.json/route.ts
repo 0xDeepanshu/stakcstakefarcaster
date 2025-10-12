628ef9d7c70fe29811c8a8948b98bad6be641706
0xDeepanshu <wtfdeep@duck.com>
2025-10-12 22:27:30 +0530

    fixed errors

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -3,7 +3,11 @@ import { APP_URL } from "../../../lib/constants";
 
 export async function GET() {
   const farcasterConfig = {
-    
+    "accountAssociation": {
+    "header": "eyJmaWQiOjEzNzUzNjIsInR5cGUiOiJhdXRoIiwia2V5IjoiMHg4RDdiQzIxMDM2ZkZCQkU3NjQ3MEM1Yjk4QTFhNDBkOTI5M0E2NjAyIn0",
+    "payload": "eyJkb21haW4iOiJzdGFrY3N0YWtlZmFyY2FzdGVyLnZlcmNlbC5hcHAifQ",
+    "signature": "+Z/k171REdkBHDohAtqNctS0oFaXfBuzNGatgD1aWwVF0TBIIaEqf8O4z1MlUhGmr79e5s7MIMO4ahPrIaDJGxs="
+  },
     "miniapp": {
       version: "1",
       name: "Rupture Labs mini app",
@@ -12,7 +16,7 @@ export async function GET() {
       splashImageUrl: `${APP_URL}/images/BlackRL.png`,
       splashBackgroundColor: "#ffffff",
       webhookUrl: `${APP_URL}/api/webhook`,
-      subtitle: "experience a good gaming with rl games and out mini game as its highly preserved",
+      subtitle: "Experience gaming with RL",
       description: "A template for building Farcaster Mini Apps with Monad integration",
       primaryCategory: "developer-tools",
       tags: ["monad", "farcaster", "miniapp", "template"],
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import App from '@/components/pages/app'
 import { APP_URL } from '@/lib/constants'
 import type { Metadata } from 'next'
-import FrontEnd from '@/components/pages/main'
-import { useMiniApp } from '@neynar/react';
 const miniapp = {
-  version: '1',
+  version: 'next',
   imageUrl: `${APP_URL}/images/EVMcd.png`,
   button: {
     title: 'Launch Template',
@@ -19,8 +17,6 @@ const miniapp = {
 }
 
 export async function generateMetadata(): Promise<Metadata> {
-  const { actions, added, notificationDetails, haptics, context } =
-    useMiniApp();
   return {
     title: 'Rupture Labs mini app ',
     openGraph: {
